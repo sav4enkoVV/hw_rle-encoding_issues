17701858831a979c1766f53e77f6f090b89b3f1f
valera <79515414890@yandex.ru>
2021-11-29 11:36:32 +0300

    fix test code

--- src/test/scala/RleEncodingSpec.scala
+++ src/test/scala/RleEncodingSpec.scala
@@ -9,15 +9,20 @@ class RleEncodingSpec extends AnyFlatSpec with should.Matchers {
   "RleEncoding" should "encode given string" in {
     val str = "XAAABBBCCCX"
     rleEncodingSpec.encode(str) should be (List(UncompressedBlock(1,List('X')), CompressedBlock(3,'A'), CompressedBlock(3,'B'), CompressedBlock(3,'C'), UncompressedBlock(1,List('X'))))
-    val str1 = "XYXYYYZ"
-    rleEncodingSpec.encode(str1) should be (List(UncompressedBlock(3,List('X','Y','X')), CompressedBlock(3,'Y'), UncompressedBlock(1,List('Z'))))
-    val str2 = "AABCDYYZXX"
-    rleEncodingSpec.encode(str2) should be (List(CompressedBlock(2,'A'), UncompressedBlock(3,List('B','C','D')), CompressedBlock(2,'Y'), UncompressedBlock(1,List('Z')), CompressedBlock(2,'X')))
-    val str3 = "XXXXZZYYYYA"
-    rleEncodingSpec.encode(str3) should be (List(CompressedBlock(4,'X'), CompressedBlock(2,'Z'), CompressedBlock(4,'Y'), UncompressedBlock(1,List('A'))))
   }
 
-  it should "" in {
-    ???
+  it should "encode given string_1" in {
+    val str = "XYXYYYZ"
+    rleEncodingSpec.encode(str) should be (List(UncompressedBlock(3,List('X','Y','X')), CompressedBlock(3,'Y'), UncompressedBlock(1,List('Z'))))
+  }
+
+  it should "encode given string_2" in {
+    val str = "AABCDYYZXX"
+    rleEncodingSpec.encode(str) should be (List(CompressedBlock(2,'A'), UncompressedBlock(3,List('B','C','D')), CompressedBlock(2,'Y'), UncompressedBlock(1,List('Z')), CompressedBlock(2,'X')))
+  }
+
+  it should "encode given string_3" in {
+    val str = "XXXXZZYYYYA"
+    rleEncodingSpec.encode(str) should be (List(CompressedBlock(4,'X'), CompressedBlock(2,'Z'), CompressedBlock(4,'Y'), UncompressedBlock(1,List('A'))))
   }
 }
