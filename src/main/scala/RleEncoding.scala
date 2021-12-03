0557e7da34c9723943b65592d2f63d7779c2c3ae
Valera <79515414890@yandex.ru>
2021-12-03 21:34:23 +0300

    fix code

--- src/main/scala/RleEncoding.scala
+++ src/main/scala/RleEncoding.scala
@@ -1,4 +1,4 @@
-import scala.collection.mutable.Stack
+import scala.collection.mutable.IndexedSeq
 
 object RleEncodingApp extends App {
 
@@ -17,14 +17,14 @@ trait Block {
   def length: Int
 }
 
-case class UncompressedBlock(length: Int, data: Stack[Char]) extends Block
+case class UncompressedBlock(length: Int, data: IndexedSeq[Char]) extends Block
 case class CompressedBlock(length: Int, data: Char) extends Block
 
 class RleEncoding {
 
-  def encode(str: String): Stack[Block] = {
+  def encode(str: String): IndexedSeq[Block] = {
     val (prev, optBlock, result) =
-      str.toCharArray.foldLeft((None: Option[Char], None: Option[Block], Stack.empty[Block])) {
+      str.toCharArray.foldLeft((None: Option[Char], None: Option[Block], IndexedSeq.empty[Block])) {
         case ((None, _, result), char) =>
           (Some(char), None, result)
 
@@ -32,27 +32,27 @@ class RleEncoding {
             (Some(char), Some(CompressedBlock(1, prev)), result)
 
         case ((Some(prev), None, result), char) =>
-            (Some(char), Some(UncompressedBlock(1, Stack(prev))), result)
+            (Some(char), Some(UncompressedBlock(1, IndexedSeq(prev))), result)
 
         case ((Some(prev), Some(block@CompressedBlock(_, _)), result), char) if prev == char =>
           (Some(char), Some(CompressedBlock(block.length + 1, block.data)), result)
 
         case ((Some(prev), Some(block@CompressedBlock(_, _)), result), char) =>
-          (Some(char), None, result += CompressedBlock(block.length + 1, block.data))
+          (Some(char), None, result :+ CompressedBlock(block.length + 1, block.data))
 
         case ((Some(prev), Some(block@UncompressedBlock(_, _)), result), char) if prev != char =>
-          (Some(char), Some(UncompressedBlock(block.length + 1, block.data += prev)), result)
+          (Some(char), Some(UncompressedBlock(block.length + 1, block.data :+ prev)), result)
 
         case ((Some(prev), Some(block@UncompressedBlock(_, _)), result), char) =>
-          (Some(char), Some(CompressedBlock(1, prev)), result += block)
+          (Some(char), Some(CompressedBlock(1, prev)), result :+ block)
       }
 
 
     (optBlock, prev) match {
-      case (Some(block@UncompressedBlock(_, _)), Some(prev)) => result += UncompressedBlock(block.length + 1, block.data += prev)
-      case (Some(block@CompressedBlock(_, data)), Some(prev)) => result += CompressedBlock(block.length + 1, data)
-      case (None, Some(prev)) => result += UncompressedBlock(1, Stack(prev))
-      case (None, None) => Stack.empty[Block]
+      case (Some(block@UncompressedBlock(_, _)), Some(prev)) => result :+ UncompressedBlock(block.length + 1, block.data :+ prev)
+      case (Some(block@CompressedBlock(_, data)), Some(prev)) => result :+ CompressedBlock(block.length + 1, data)
+      case (None, Some(prev)) => result :+ UncompressedBlock(1, IndexedSeq(prev))
+      case (None, None) => IndexedSeq.empty[Block]
     }
 
   }
--- src/test/scala/RleEncodingSpec.scala
+++ src/test/scala/RleEncodingSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 import flatspec._
 import matchers._
 
-import scala.collection.mutable.Stack
+import scala.collection.mutable.ArrayBuffer
 
 
 class RleEncodingSpec extends AnyFlatSpec with should.Matchers {
@@ -11,38 +11,38 @@ class RleEncodingSpec extends AnyFlatSpec with should.Matchers {
 
   "RleEncoding" should "encode given string" in {
     val str = "XAAABBBCCCX"
-    rleEncodingSpec.encode(str) should be (Stack(
-      UncompressedBlock(1,Stack('X')),
+    rleEncodingSpec.encode(str) should be (ArrayBuffer(
+      UncompressedBlock(1,ArrayBuffer('X')),
       CompressedBlock(3,'A'),
       CompressedBlock(3,'B'),
       CompressedBlock(3,'C'),
-      UncompressedBlock(1,Stack('X'))))
+      UncompressedBlock(1,ArrayBuffer('X'))))
   }
 
   it should "encode given string_1" in {
     val str = "XYXYYYZ"
-    rleEncodingSpec.encode(str) should be (Stack(
-      UncompressedBlock(3,Stack('X','Y','X')),
+    rleEncodingSpec.encode(str) should be (ArrayBuffer(
+      UncompressedBlock(3,ArrayBuffer('X','Y','X')),
       CompressedBlock(3,'Y'),
-      UncompressedBlock(1,Stack('Z'))))
+      UncompressedBlock(1,ArrayBuffer('Z'))))
   }
 
   it should "encode given string_2" in {
     val str = "AABCDYYZXX"
-    rleEncodingSpec.encode(str) should be (Stack(
+    rleEncodingSpec.encode(str) should be (ArrayBuffer(
       CompressedBlock(2,'A'),
-      UncompressedBlock(3,Stack('B','C','D')),
+      UncompressedBlock(3,ArrayBuffer('B','C','D')),
       CompressedBlock(2,'Y'),
-      UncompressedBlock(1,Stack('Z')),
+      UncompressedBlock(1,ArrayBuffer('Z')),
       CompressedBlock(2,'X')))
   }
 
   it should "encode given string_3" in {
     val str = "XXXXZZYYYYA"
-    rleEncodingSpec.encode(str) should be (Stack(
+    rleEncodingSpec.encode(str) should be (ArrayBuffer(
       CompressedBlock(4,'X'),
       CompressedBlock(2,'Z'),
       CompressedBlock(4,'Y'),
-      UncompressedBlock(1,Stack('A'))))
+      UncompressedBlock(1,ArrayBuffer('A'))))
   }
 }
